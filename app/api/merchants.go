b09860419e7867e2deaaefdc8a29ca2ede08af60
yasinta musa <yasinta@sepulsa.com>
2019-07-26 10:12:25 +0700

    [#167287561] add router merchant

--- app/api/merchants.go
+++ app/api/merchants.go
@@ -2,20 +2,23 @@ package api
 
 import (
 	"errors"
+
+	"encoding/json"
 	"net/http"
 
 	"github.com/ztrue/tracerr"
 	"gopkg.in/mgo.v2/bson"
 
+	"github.com/hilmansyafei/canopus-master-mservice/database/models"
 	"github.com/hilmansyafei/go-package/response"
 	"github.com/hilmansyafei/go-package/status"
 	"github.com/labstack/echo"
 )
 
-// GetMerchantByIDHandler : api handler.
+// GetMerchantByIDHandler : api handler get data by Id from merchants collection
 func (h *Handler) GetMerchantByIDHandler(c echo.Context) error {
-	ID := c.Param("mid")
-	if !bson.IsObjectIdHex(ID) {
+	_id := c.Param("id")
+	if !bson.IsObjectIdHex(_id) {
 		sErr := response.BuildError(response.NewErrorInfo(
 			"Canopus - Response: [GetMerchantByIDHandler] function",
 			"Invalid PID format",
@@ -24,13 +27,14 @@ func (h *Handler) GetMerchantByIDHandler(c echo.Context) error {
 		c.JSON(http.StatusInternalServerError, sErr)
 		return tracerr.Wrap(errors.New("Invalid PID format"))
 	}
-	merchants, err := h.Repositories.GetMerchantByID(ID)
+
+	merchants, err := h.Repositories.GetMerchantByID(_id)
 	if err != nil {
 		if err.Error() == "not found" {
-			sSuccess := response.BuildSuccess("Data Not Found", status.DataNotFound)
+			sSuccess := response.BuildSuccess("Merchant Not Found", status.OKSuccess)
 			GenLog(c, "", sSuccess, "Response Log")
 			c.JSON(http.StatusNotFound, sSuccess)
-			return tracerr.Wrap(err)
+			return nil
 		}
 		// Database error
 		sErr := response.BuildError(response.NewErrorInfo(
@@ -43,6 +47,158 @@ func (h *Handler) GetMerchantByIDHandler(c echo.Context) error {
 	}
 
 	sSuccess := response.BuildSuccess(merchants, status.OKSuccess)
+	GenLog(c, "", sSuccess, "Response Log")
+	c.JSON(http.StatusOK, sSuccess)
+	return nil
+}
+
+// GetAllMerchant : api handler to get all data from merchants collection
+func (h *Handler) GetAllMerchant(c echo.Context) error {
+	var merchants []interface{}
+	err := h.Repositories.GetAllMerchant(&merchants)
+
+	if err != nil {
+		// Database error
+		sErr := response.BuildError(response.NewErrorInfo(
+			"Canopus - Response: [GetAll] function",
+			"Database Error",
+			"app/api/merchants.go"), status.InternalServerError)
+		GenLog(c, "", sErr, "Error Log")
+		c.JSON(http.StatusInternalServerError, sErr)
+		return tracerr.Wrap(err)
+	}
+
+	sSuccess := response.BuildSuccess(merchants, status.OKSuccess)
+	GenLog(c, "", sSuccess, "Response Log")
+	c.JSON(http.StatusOK, sSuccess)
+	return nil
+}
+
+// CreateMerchant : api handler.
+func (h *Handler) CreateMerchant(c echo.Context) error {
+
+	jsonMap := make(map[string]interface{})
+	err := json.NewDecoder(c.Request().Body).Decode(&jsonMap)
+	if err != nil {
+		return err
+	}
+
+	statusInt := int(jsonMap["Status"].(float64))
+	statusInt32 := int32(statusInt)
+
+	env32 := int(jsonMap["ENV"].(float64))
+	thisEnv := int32(env32)
+
+	//json_map has the JSON Payload decoded into a map
+	data := models.Merchants{
+		ShortName:        jsonMap["ShortName"].(string),
+		OfficialName:     jsonMap["OfficialName"].(string),
+		Email:            jsonMap["Email"].(string),
+		Status:           statusInt32,
+		MID:              jsonMap["MID"].(string),
+		ENV:              thisEnv,
+		SecretKey:        jsonMap["SecretKey"].([]interface{}),
+		MerchantPubKey:   bson.ObjectIdHex(jsonMap["MerchantPubKey"].(string)),
+		PsaPrivKey:       bson.ObjectIdHex(jsonMap["PsaPrivKey"].(string)),
+		PsaPubKey:        bson.ObjectIdHex(jsonMap["PsaPubKey"].(string)), // bson.NewObjectId(),
+		Method:           jsonMap["Method"].([]interface{}),
+		NotificationURLs: jsonMap["NotificationURLs"].(interface{}),
+		WhitelistIP:      jsonMap["WhitelistIP"].(interface{}),
+	}
+
+	err = h.Repositories.CreateMerchant(data)
+	if err != nil {
+		// Database error
+		sErr := response.BuildError(response.NewErrorInfo(
+			"Canopus - Response: [CreateMerchant] function",
+			"Database Error",
+			"app/api/merchant.go"), status.InternalServerError)
+		GenLog(c, "", sErr, "Error Log")
+		c.JSON(http.StatusInternalServerError, sErr)
+		return tracerr.Wrap(err)
+	}
+	sSuccess := response.BuildSuccess("Successfully create merchant", status.OKSuccess)
+	GenLog(c, "", sSuccess, "Response Log")
+	c.JSON(http.StatusOK, sSuccess)
+	return nil
+}
+
+// UpdateMerchant : api handler.
+func (h *Handler) UpdateMerchant(c echo.Context) error {
+	jsonMap := make(map[string]interface{})
+	err := json.NewDecoder(c.Request().Body).Decode(&jsonMap)
+	if err != nil {
+		return err
+	}
+	statusInt := int(jsonMap["Status"].(float64))
+	statusInt32 := int32(statusInt)
+
+	env32 := int(jsonMap["ENV"].(float64))
+	thisEnv := int32(env32)
+
+	//json_map has the JSON Payload decoded into a map
+	dataUpdate := models.Merchants{
+		ShortName:        jsonMap["ShortName"].(string),
+		OfficialName:     jsonMap["OfficialName"].(string),
+		Email:            jsonMap["Email"].(string),
+		Status:           statusInt32,
+		MID:              jsonMap["MID"].(string),
+		ENV:              thisEnv,
+		SecretKey:        jsonMap["SecretKey"].([]interface{}),
+		MerchantPubKey:   bson.ObjectIdHex(jsonMap["MerchantPubKey"].(string)),
+		PsaPrivKey:       bson.ObjectIdHex(jsonMap["PsaPrivKey"].(string)),
+		PsaPubKey:        bson.ObjectIdHex(jsonMap["PsaPubKey"].(string)), // bson.NewObjectId(),
+		Method:           jsonMap["Method"].([]interface{}),
+		NotificationURLs: jsonMap["NotificationURLs"].(interface{}),
+		WhitelistIP:      jsonMap["WhitelistIP"].(interface{}),
+	}
+	_id := c.Param("id")
+	err3 := h.Repositories.UpdateMerchant(_id, dataUpdate)
+
+	if err3 != nil {
+		if err3.Error() == "not found" {
+			sSuccess := response.BuildSuccess("Data Not Found", status.OKSuccess)
+			GenLog(c, "", sSuccess, "Response Log")
+			c.JSON(http.StatusNotFound, sSuccess)
+			return nil
+		}
+		// Database error
+		sErr := response.BuildError(response.NewErrorInfo(
+			"Canopus - Response: [UpdateMerchant] function",
+			"Database Error",
+			"app/api/merchant.go"), status.InternalServerError)
+		GenLog(c, "", sErr, "Error Log")
+		c.JSON(http.StatusInternalServerError, sErr)
+		return tracerr.Wrap(err)
+	}
+	sSuccess := response.BuildSuccess("Successfully update merchant", status.OKSuccess)
+	GenLog(c, "", sSuccess, "Response Log")
+	c.JSON(http.StatusOK, sSuccess)
+	return nil
+}
+
+// DeleteMerchantByID : api handler to delete data from merchants collection
+func (h *Handler) DeleteMerchantByID(c echo.Context) error {
+	_id := c.Param("id")
+	err := h.Repositories.DeleteMerchantByID(_id)
+	if err != nil {
+		if err.Error() == "not found" {
+			sSuccess := response.BuildSuccess("Merchant Not Found", status.OKSuccess)
+			GenLog(c, "", sSuccess, "Response Log")
+			c.JSON(http.StatusNotFound, sSuccess)
+			return nil
+		}
+		// Database error
+		sErr := response.BuildError(response.NewErrorInfo(
+			"Canopus - Response: [GetMerchantByID] function",
+			"Database Error",
+			"app/api/merchant.go"), status.InternalServerError)
+		GenLog(c, "", sErr, "Error Log")
+		c.JSON(http.StatusInternalServerError, sErr)
+		return tracerr.Wrap(err)
+	}
+
+	sSuccess := response.BuildSuccess("Successfully delete merchant", status.OKSuccess)
 	GenLog(c, "", sSuccess, "Response Log")
 	c.JSON(http.StatusOK, sSuccess)
 	return nil
--- routers/api.go
+++ routers/api.go
@@ -12,15 +12,22 @@ func Gen(h *api.Handler) *echo.Echo {
 	//Initiate router
 	e := echo.New()
 
-	//Add router
+	// Condition router
 	e.GET("/conditionByPID/:pid", h.GetConditionByPID, middleware.Track)
 	e.GET("/conditionByID/:id", h.GetConditionByID, middleware.Track)
 	e.GET("/conditionAll", h.GetConditionAll, middleware.Track)
 	e.GET("/condition/:pid/:event", h.GetCondition, middleware.Track)
-	e.GET("/merchantByID/:mid", h.GetMerchantByIDHandler, middleware.Track)
+
+	// Merchant Routes
+	e.GET("/merchant/:id", h.GetMerchantByIDHandler, middleware.Track)
+	e.POST("/merchant", h.CreateMerchant, middleware.Track)
+	e.DELETE("/merchant/:id", h.DeleteMerchantByID, middleware.Track)
+	e.GET("/merchant", h.GetAllMerchant, middleware.Track)
+	e.PUT("/merchant/:id", h.UpdateMerchant, middleware.Track)
 
 	// Files Routes.
 	e.GET("/getPathFile/:mid", h.GetPathFileHandler, middleware.Track)
+	e.GET("/getAllFiles", h.GetAllFiles, middleware.Track)
 
 	e.HTTPErrorHandler = middleware.CustomHTTPErrorHandler
 
